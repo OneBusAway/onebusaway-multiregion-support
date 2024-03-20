034f87ba04767455e6d55ee16a299fdbd156ac21
Aaron Brethorst <aaron@brethorsting.com>
2024-03-19 22:46:34 -0700

    Move csv reader functionality into its own file

--- update_regions.py
+++ update_regions.py
@@ -17,10 +17,8 @@ limitations under the License.
 """
 
 import argparse
-import csv
-import io
 import sys
-import urllib.request, urllib.error, urllib.parse
+from src.csv_readers import get_csv_from_file, get_csv_from_url
 from src.serializers import JSONSerializer, XMLSerializer, serialize
 from src.writers import output_file, output_s3, output_stdout
 
@@ -54,27 +52,6 @@ parser.add_argument('--pretty',
                     action='store_true',
                     default=False,
                     help='Make the output files pretty and readable with indentation.')
-
-
-# In both reading cases, we need to read all data into memory
-# because there's no way of seeking back in a CSV reader.
-
-def get_csv_from_file(path):
-    with open(path) as f:
-        reader = csv.DictReader(open(path))
-        return list(reader)
-
-
-def get_csv_from_url(url):
-    "Returns a list of regions from the specified spreadsheet URL."
-    opener = urllib.request.build_opener(
-        urllib.request.HTTPCookieProcessor(),
-        urllib.request.HTTPRedirectHandler()
-    )
-    urllib.request.install_opener(opener)
-    response = urllib.request.urlopen(url)
-    reader = csv.DictReader(io.TextIOWrapper(response, encoding='utf-8'))
-    return list(reader)
 
 
 def main():
